6f0f27c4e1a535ca629bc57732b4ec7483dae253
Maja090 <worldhappy090@gmail.com>
2019-08-27 19:59:37 +0200

    Complete

---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/ui/camera/MainActivity.java
@@ -0,0 +1,17 @@
+package com.google.android.gms.samples.vision.face.facetracker.ui.camera;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+/**
+ * Created by adithya on 1/5/16.
+ */
+public class MainActivity  {
+
+    private SensorManager SM;
+    private Sensor mSensor;
+
+    SM = (SensorManager)getSystemService();
+
+
+}
